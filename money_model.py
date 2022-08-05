b976bf79d03c779338aac49cd85339f53651247a
skitalets <ctessone@Christophers-MacBook-Pro.local>
2022-08-05 14:32:33 -0400

    End of introductory tutorial, working model with Gini collection

--- money_model.py
+++ money_model.py
@@ -1,5 +1,13 @@
 import mesa
 
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
+
+    return 1 + (1 / N) - 2 * B
+
 class MoneyAgent(mesa.Agent):
     """An agent with fixed initial wealth."""
 
@@ -31,8 +39,6 @@ class MoneyAgent(mesa.Agent):
         if self.wealth > 0:
             self.give_money()
 
-        print("Hi, I am agent " + str(self.unique_id) + " and my wealth is " + str(self.wealth) + ".")
-
 class MoneyModel(mesa.Model):
     """A model with some number of agents."""
 
@@ -40,6 +46,7 @@ class MoneyModel(mesa.Model):
         self.num_agents = N
         self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
+        self.running = True
 
         # Create agents
         for i in range(self.num_agents):
@@ -51,7 +58,12 @@ class MoneyModel(mesa.Model):
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
 
+        self.datacollector = mesa.DataCollector (
+            model_reporters = {"Gini": compute_gini}, agent_reporters = {"Wealth": "wealth"}
+        )
+
     def step(self):
         """Advance the model one step."""
 
+        self.datacollector.collect(self)
         self.schedule.step()

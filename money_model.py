0a13756b798c961697160f71cfd729d47c519308
skitalets <ctessone@Christophers-MacBook-Pro.local>
2022-08-05 10:42:26 -0400

    Next model with movement

--- money_model.py
+++ money_model.py
@@ -7,30 +7,49 @@ class MoneyAgent(mesa.Agent):
         super().__init__(unique_id, model)
         self.wealth = 1
 
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore = True,
+            include_center = False
+        )
+
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def give_money(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other = self.random.choice(cellmates)
+            other.wealth = other.wealth + 1
+            self.wealth = self.wealth - 1
+
     def step(self):
         # The agent's step will go here.
         # For demonstration purposes we will print the agent's unique_id
 
-        if self.wealth == 0:
-            return
-
-        other_agent = self.random.choice(self.model.schedule.agents)
-        other_agent.wealth = other_agent.wealth + 1
-        self.wealth = self.wealth - 1
+        if self.wealth > 0:
+            self.give_money()
 
         print("Hi, I am agent " + str(self.unique_id) + " and my wealth is " + str(self.wealth) + ".")
 
 class MoneyModel(mesa.Model):
     """A model with some number of agents."""
 
-    def __init__(self, N):
+    def __init__(self, N, width, height):
         self.num_agents = N
+        self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
 
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
             self.schedule.add(a)
+
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
 
     def step(self):
         """Advance the model one step."""
